7b9ace840f0f184348c1701873f62c0c9c6eb125
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-09 14:03:34 +0100

    bs style

--- index.php
+++ index.php
@@ -4,22 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Bad Words</title>
 </head>
 <body>
-    <main>
-        <p>
-            <?php
-                $testo = "asd";
-                echo $testo;
-            ?>
-        </p>
-        <span>Il totale dei caratteri é:</span>
-        <span>
-            <?php
-                echo strlen($testo);
-            ?> 
-        </span>
+    <main >
+        <section class="d-flex justify-content-center align-items-center flex-column">
+            <span class="fs-3"> Questo è la stringa da contare: </span>
+            <p class="fs-3 text-center text-success fw-bold">
+                <?php
+                    $testo = "asd";
+                    echo $testo;
+                ?>
+            </p>
+            <span class="fs-3">Il totale dei caratteri é:</span>
+            <span class="fs-3 fw-bold text-danger">
+                <?php
+                    echo strlen($testo);
+                ?> 
+            </span>
+        </section>
 
     </main>
 </body>
